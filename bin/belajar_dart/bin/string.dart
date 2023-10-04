0b0506b855670ca663976703e3620dd37fdd4653
desyariynati <146171696+desyariynati@users.noreply.github.com>
2023-10-03 23:48:45 -0700

    hghgvj

---
+++ bin/belajar_dart/bin/string.dart
@@ -0,0 +1,14 @@
+void main(){
+  String nama ="Desy";
+  int umur 19;
+  bool isBelummenikah = true;
+
+  final array1 =[1,2,3]
+  array[1]
+
+  print("nama saya $nama");
+  print("nama saya $umur");
+  print("saya belum menikah $isBelummenikah");
+  print("array saya adalah $getArray");
+
+}
